3ffbaf4780ec44ad87ec28d36c64d3b5f0484eac
Henrique Melo Lea <Henriqueml@academicos.com>
2024-04-25 20:59:32 -0300

    Adicionando o Código do App

--- study-app/src/contexts/StudyCardsContext.js
+++ study-app/src/contexts/StudyCardsContext.js
@@ -1,40 +1,32 @@
-import React, {createContext, useState, useEffect} from 'react'
-
+import React, { createContext, useState, useEffect } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const StudyCardsContext = createContext()
 
-export const StudyCardsProvider = ({children}) => {
+export const StudyCardsProvider = ({ children }) => {
     const [cards, setCards] = useState([])
-    
-    useEffect (() => {
 
-        LoadCards()
-
+    useEffect(() => {
+        loadCards()
     }, [])
 
-    const LoadCards = async () => {
+    const loadCards = async () => {
         const storedCards = await AsyncStorage.getItem('cards')
-
         if (storedCards) setCards(JSON.parse(storedCards))
-s
     }
 
     const addCard = async (card) => {
-
-        const newCards = [...cards, {...card, id: Date.now()}]
+        const newCards = [...cards, { ...card, id: Date.now() }]
         setCards(newCards)
-        await AsyncStorage.setItem('cards', JSON.stringfy(newCards))
-
+        await AsyncStorage.setItem('cards', JSON.stringify(newCards))
     }
 
     const updateCard = async (id, updates) => {
         const newCards = cards.map(card => card.id === id ? {
             card, ...updates
-        } : card )
+        } : card)
         setCards(newCards)
         await AsyncStorage.setItem('cards', JSON.stringify(newCards))
-
     }
 
     const deleteCard = async (id) => {
@@ -44,12 +36,11 @@ s
     }
 
     return (
-        <StudyCardsProvider.Provider value = {{ cards, addCard, updateCard, deleteCard  }}>
-           {children} 
-        </StudyCardsProvider.Provider>
+        <StudyCardsContext.Provider value={{ cards, addCard, updateCard, deleteCard }}>
+            {children}
+        </StudyCardsContext.Provider>
     )
 
 }
 
 export default StudyCardsContext
-
